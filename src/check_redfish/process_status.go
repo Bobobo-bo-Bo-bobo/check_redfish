7f00498567e85810ddddf0393d70defa69454cae
Andreas Maus <maus@badphish.ypbind.de>
2018-12-10 09:59:13 +0100

    Fix message generation

--- src/check_redfish/process_status.go
+++ src/check_redfish/process_status.go
@@ -1,26 +1,57 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 )
 
 func ProcessStatus(n NagiosState) (int, string) {
+	var rc int = NAGIOS_UNKNOWN
+	var msg string
 	u_str := strings.Join(n.Unknown, ", ")
 	c_str := strings.Join(n.Critical, ", ")
 	w_str := strings.Join(n.Warning, ", ")
 	o_str := strings.Join(n.Ok, ", ")
 
 	if len(n.Unknown) > 0 {
-		return NAGIOS_UNKNOWN, fmt.Sprintf("%s; %s; %s; %s", u_str, c_str, w_str, o_str)
+		rc = NAGIOS_UNKNOWN
+		msg = u_str
+		if len(c_str) > 0 {
+			msg += "; " + c_str
+		}
+
+		if len(w_str) > 0 {
+			msg += "; " + w_str
+		}
+
+		if len(o_str) > 0 {
+			msg += "; " + o_str
+		}
 	} else if len(n.Critical) > 0 {
-		return NAGIOS_CRITICAL, fmt.Sprintf("%s; %s; %s", c_str, w_str, o_str)
+		rc = NAGIOS_CRITICAL
+		msg = c_str
+
+		if len(w_str) > 0 {
+			msg += "; " + w_str
+		}
+
+		if len(o_str) > 0 {
+			msg += "; " + o_str
+		}
 	} else if len(n.Warning) > 0 {
-		return NAGIOS_WARNING, fmt.Sprintf("%s; %s", w_str, o_str)
+		rc = NAGIOS_WARNING
+		msg = w_str
+
+		if len(o_str) > 0 {
+			msg += "; " + o_str
+		}
 	} else if len(n.Ok) > 0 {
-		return NAGIOS_OK, fmt.Sprintf("%s", o_str)
+		rc = NAGIOS_OK
+		msg = o_str
 	} else {
 		// shouldn't happen
-		return NAGIOS_UNKNOWN, "No results at all found"
+		rc = NAGIOS_UNKNOWN
+		msg = "No results at all found"
 	}
+
+	return rc, msg
 }
